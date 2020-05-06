8da207caa9408f1feb38fdf3031c8f8764fec2c4
Kristin Dona <donakristin@gmail.com>
2020-05-06 13:35:52 -0400

    add colab files (not all have compatibility with the libraries yet) to this repository

--- THz-FTS/README.md
+++ THz-FTS/README.md
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
+# BREAD_exp
+# BREAD_exp
+=======
 # THz-FTS
 Code for simulating/analyzing output from the BREAD THz FTS
+>>>>>>> 82850ac60cc582f4bb9ae817da37778a549968d3
--- THz-FTS/python/lib/FTSFunctions.py
+++ THz-FTS/python/lib/FTSFunctions.py
@@ -1,6 +1,7 @@
 from scipy.signal import periodogram as psd
 import numpy as np
 
+L=12e4
 dL = 0.1 #micron, step size
 fs = 1/dL #micron^-1, inv. step size (multiply by h gives energy resolution)
 c_micron_per_second = 3.0e8 * 1e6
@@ -17,4 +18,13 @@ def adjust_spectrum(spectrum, adjustment=1e-7):
         if adjusted_spectrum[p] < 0:
           adjusted_spectrum[p] = 0
     return adjusted_spectrum
+
+def getFreqList_fromPSD(L=L):
+    y=np.arange(0,L,dL)
+    wave_number, dummy_spectrum = psd(y,fs=fs)
+    freq = wave_number*c_micron_per_second*Hz_to_THz
+    frequency_slice_size = abs(freq[0]-freq[1]) #slice size in THz, doesnt change throughout the arrange so we can just use the first separation
+    #create seprate array to avoid the issue of first entry being 0
+    frequencies = np.arange(frequency_slice_size, max(freq), frequency_slice_size) #still in THz (smallest entry is slice size to avoid 0)
+    return frequencies, frequency_slice_size
 
